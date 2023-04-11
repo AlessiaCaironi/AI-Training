37a881acd9baf0f8c3579fe733ff1edc66d2f9a1
Alessia Caironi <alessia.caironi@orobix.com>
2023-04-11 11:49:28 +0200

    hide-secret-key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+# recupera variabili globali dal file .env
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8qp-i7-q%wi38i*qdtmia0#fj7l$rc=px1c17%(!f*rs1-w-+d'
+# recupero SECRET_KEY dal file .env
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
